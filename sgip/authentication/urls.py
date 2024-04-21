eed24416b78a2c6c14d94ac0e70d967a7f5fd9c9
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2024-04-21 12:20:37 -0300

    Update urls.py

--- sgip/authentication/urls.py
+++ sgip/authentication/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
 	path("", views.home, name="home"),
-	path("new-product/", views.new_product, name="new-product"),
-	path("products/", views.display_products, name="products")
+	path("products/", views.display_products, name="list-products"),
+	re_path(r'^(?P<category>[^/]+)/new-product/$', views.new_product, name='new_product'),
+	re_path(r'^(?P<category>[^/]+)/(?P<subcategory>[^/]+)/new-product/$', views.new_product, name='new_product_with_subcategory')
 ]
