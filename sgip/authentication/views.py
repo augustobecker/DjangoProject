1046da08b723ac5cb49712aa4bb1d71f4c62f855
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2024-04-21 12:21:07 -0300

    Update views.py

--- sgip/authentication/views.py
+++ sgip/authentication/views.py
@@ -1,24 +1,46 @@
 from django.shortcuts import render, redirect, HttpResponse
-from .forms import ProdutoForm
-from .models import Produto
+from urllib.parse import unquote
+from .forms import ProductForm
+from .models import Product, Category
 
 
 def home(request):
-	return render(request, "home.html")
+    categories = Category.objects.filter(parent_category__isnull=True)
+    return render(request, 'home.html', {'categories': categories})
 
 
-def new_product(request):
+def new_product(request, category, subcategory=None):
+    try:
+        category_obj = Category.objects.get(name=unquote(category))
+    except Category.DoesNotExist:
+        return HttpResponse(f'Category not found - {category}')
+
+    if subcategory:
+        try:
+            subcategory_obj = category_obj.subcategories.get(name=unquote(subcategory))
+            description = subcategory_obj.description
+        except Category.DoesNotExist:
+            return HttpResponse(f'Subcategory not found - {subcategory}')
+    else:
+        description = category_obj.description
+
     if request.method == 'POST':
-        form = ProdutoForm(request.POST)
+        form = ProductForm(request.POST)
         if form.is_valid():
-            form.save()
+            product = form.save(commit=False)
+            product.category = category_obj
+            product.save()
             return redirect('/products')
     else:
-        form = ProdutoForm()
+        initial_data = {'category': category_obj, 'description': description}
+        if subcategory:
+            initial_data['subcategory'] = subcategory_obj.name
+        form = ProductForm(initial=initial_data)
 
-    return render(request, 'cadastrate.html', {'form': form})
+    return render(request, 'cadastrate.html', {'form': form, 'category': category, 'subcategory': subcategory})
 
 
 def display_products(request):
-    products = Produto.objects.all()
-    return render(request, 'display.html', {'produtos': products})
+    products = Product.objects.all()
+    return render(request, 'display.html', {'products': products})
+
