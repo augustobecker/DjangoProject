ed5909e7f223e721ee9b6ecb56d4e85cba0e981c
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2024-04-21 12:20:16 -0300

    Update models.py

--- sgip/authentication/models.py
+++ sgip/authentication/models.py
@@ -1,10 +1,26 @@
 from django.db import models
 
-class Produto(models.Model):
-    id = models.AutoField(primary_key=True)
-    nome = models.CharField(max_length=100)
-    descricao = models.TextField()
-    preco = models.DecimalField(max_digits=10, decimal_places=2)
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    formal_name = models.CharField(max_length=100)
+    parent_category = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='subcategories')
+    description = models.TextField(default=1)
+
+    def save(self, *args, **kwargs):
+        if not self.formal_name:sa
+            self.formal_name = self.name.capitalize()
+        super().save(*args, **kwargs)
 
     def __str__(self):
-        return self.nome
+        return self.name
+
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+
+    def __str__(self):
+        return self.name
